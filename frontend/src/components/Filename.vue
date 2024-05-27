530e68341398663f2bc43d5c82baa4b3b133b5cd
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-05-27 10:21:14 +0000

    fix(frontend/files): Fix text fade out won't adapt to screen size bug

--- frontend/src/components/Filename.vue
+++ frontend/src/components/Filename.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Ref, ref, onMounted } from "vue";
-const props = defineProps<{filename: string}>()
+const props = defineProps<{ filename: string }>();
 
 const pClass = ref("text-body-2 tw-overflow-hidden tw-whitespace-nowrap");
 
@@ -8,25 +8,26 @@ const pTag: Ref<HTMLElement | null> = ref(null);
 const overflowing = ref(false);
 
 function checkOverflow() {
-    if(pTag.value) {
-        console.log(pTag.value);
-        if (pTag.value.scrollWidth > pTag.value.clientWidth) {
-            overflowing.value = true;
-            pClass.value += " text-fade-out";
-        }
+  if (pTag.value) {
+    console.log(pTag.value);
+    if (pTag.value.scrollWidth > pTag.value.clientWidth) {
+      overflowing.value = true;
+      pClass.value += " text-fade-out";
+    } else {
+      overflowing.value = false;
+      pClass.value = "text-body-2 tw-overflow-hidden tw-whitespace-nowrap";
     }
+  }
 }
 
 onMounted(() => {
-    checkOverflow()
-})
-
+  checkOverflow();
+  window.addEventListener("resize", checkOverflow);
+});
 </script>
 
 <template>
-    <p ref="pTag" :class="pClass">{{props.filename}}</p>
+  <p ref="pTag" :class="pClass">{{ props.filename }}</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
