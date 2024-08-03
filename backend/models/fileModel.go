28148da671bee2d5ea915819389076b86581b13c
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-03 08:02:24 +0000

    feat(database): Rework entity relationships
    - Removed FolderChild model.
    - Updated Folder model.
    - Updated Folder and Folder (subfolder) relationship.
    - Updated Folder and File (contents) relationship.
    - Updated User model where it can have many files and folders.

--- backend/models/fileModel.go
+++ backend/models/fileModel.go
@@ -5,10 +5,11 @@ import "gorm.io/gorm"
 type File struct {
 	gorm.Model
 	UserID      uint   `gorm:"not null"`
+	FolderID uint   `gorm:"not null"`
 	FileName    string `gorm:"type:varchar(255);not null"`
+	FileCode    string `gorm:"type:char(36);not null"`
 	FileSize    uint   `gorm:"not null"`
 	FileType    string `gorm:"type:varchar(100);not null"`
-	DirPath     string `gorm:"type:text;not null"`
-	StoragePath string `gorm:"type:varchar(100);not null"`
 	IsFavorite  bool   `gorm:"not null"`
+	Folder Folder `gorm:"foreignKey:FolderID"`
 }
