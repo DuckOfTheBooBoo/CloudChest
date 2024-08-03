05288a9cd11806d51cb4b628682cc87e20ac8d5d
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-03 12:23:09 +0000

    feat(database): Make gorm automatically translate errors

--- backend/database/database.go
+++ backend/database/database.go
@@ -27,7 +27,7 @@ func ConnectToDB() (Database, error) {
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
-	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{TranslateError: true})
 	if err != nil {
 		return nil, err
 	}
