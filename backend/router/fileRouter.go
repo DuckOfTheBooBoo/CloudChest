70927bc9fe95bfdff9088ce4960236f05e236ca8
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-03 12:43:50 +0000

    feat(backend): Make file router handles non-path param and path param routes

--- backend/router/fileRouter.go
+++ backend/router/fileRouter.go
@@ -10,8 +10,9 @@ func FileRoutes(route *gin.RouterGroup) {
 	file := route.Group("/files") 
 	{	
 		file.GET("", middlewares.JWTMiddleware(), controllers.FileList)
+		file.GET("/:code", middlewares.JWTMiddleware(), controllers.FileList)
 		file.POST("", middlewares.JWTMiddleware(), controllers.FileUpload)
-		file.POST("/path", middlewares.JWTMiddleware(), controllers.FileNewPath)
+		file.POST("/:code", middlewares.JWTMiddleware(), controllers.FileUpload)
 		file.PUT("/:fileID", middlewares.JWTMiddleware(), controllers.FileUpdate)
 		file.DELETE("/:fileID", middlewares.JWTMiddleware(), controllers.FileDelete)
 	}
