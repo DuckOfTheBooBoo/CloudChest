a897be12acf8e97b825e1fb4390e26c9765de334
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-04 13:57:20 +0000

    feat(frontend): Refactor file model and add Folder model

--- frontend/src/models/file.ts
+++ frontend/src/models/file.ts
@@ -1,4 +1,5 @@
 import { parseISO } from "date-fns";
+import Folder from "./folder";
 
 export interface FileResponse {
   ID: number;
@@ -6,38 +7,43 @@ export interface FileResponse {
   UpdatedAt: string;
   DeletedAt: string | null;
   UserID: number;
+  FolderID: number;
   FileName: string;
+  FileCode: string;
   FileSize: number;
-  FileType: string;
-  StoragePath: string;
+  FileType: string;  
   IsFavorite: boolean;
+  Folder: Folder | null;
 }
 
-export class MinIOFile {
+export class CloudChestFile {
   ID: number;
   CreatedAt: Date;
   UpdatedAt: Date;
   DeletedAt: Date | null;
   UserID: number;
+  FolderID: number;
   FileName: string;
+  FileCode: string;
   FileSize: number;
   FileType: string;
-  StoragePath: string;
   IsFavorite: boolean;
+
   constructor(object: FileResponse) {
     this.ID = object.ID;
     this.CreatedAt = parseISO(object.CreatedAt);
     this.UpdatedAt = parseISO(object.UpdatedAt);
     this.DeletedAt = object.DeletedAt ? parseISO(object.DeletedAt) : null;
     this.UserID = object.UserID;
+    this.FolderID = object.FolderID;
     this.FileName = object.FileName;
+    this.FileCode = object.FileCode;
     this.FileSize = object.FileSize;
     this.FileType = object.FileType;
-    this.StoragePath = object.StoragePath;
     this.IsFavorite = object.IsFavorite;
   }
 }
 
-export function parseFileResponse(response: FileResponse): MinIOFile {
-  return new MinIOFile(response);
+export function parseFileResponse(response: FileResponse): CloudChestFile {
+  return new CloudChestFile(response);
 }
