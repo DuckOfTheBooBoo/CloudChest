90f37e956abba1ccead22c7f161aae3f00895716
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-11 08:24:43 +0000

    refactor(backend): Separate generation for image and video

--- backend/jobs/thumbnailJob.go
+++ backend/jobs/thumbnailJob.go
@@ -3,32 +3,25 @@ package jobs
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"image/jpeg"
 	"log"
-	"fmt"
+	"strings"
 
 	"github.com/DuckOfTheBooBoo/web-gallery-app/backend/models"
 	"github.com/disintegration/imaging"
 	"github.com/minio/minio-go/v7"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"gorm.io/gorm"
 )
 
-func GenerateThumbnail(ctx context.Context, minioClient *minio.Client, db *gorm.DB, file models.File, userBucket string) {
-	assetFile, err := minioClient.GetObject(ctx, userBucket, file.FileCode, minio.GetObjectOptions{})
-
-	if err != nil {
-		log.Printf("Error while fetching file: %s -> %s\n", file.FileName, err.Error())
-		return
-	}
-	defer assetFile.Close()
-
+func processImage(file models.File, assetFile *minio.Object, userBucket string) (bytes.Buffer, error) {
 	assetImg, err := imaging.Decode(assetFile, imaging.AutoOrientation(true))
-	
+
 	if err != nil {
-		log.Printf("Error while decoding file: %s -> %s\n", file.FileName, err.Error())
-		return
+		return bytes.Buffer{}, fmt.Errorf("Error while decoding image file %s: %v", file.FileName, err)
 	}
-	
+
 	height := assetImg.Bounds().Dy()
 	width := assetImg.Bounds().Dx()
 
@@ -40,21 +33,50 @@ func GenerateThumbnail(ctx context.Context, minioClient *minio.Client, db *gorm.
 	// Encode the thumbImg to JPEG
 	var buf bytes.Buffer
 	if err := jpeg.Encode(&buf, thumbImg, &jpeg.Options{Quality: 85}); err != nil {
-		log.Printf("Error while processing thumbnail: %s -> %s\n", file.FileName, err.Error())
-		return
+		return bytes.Buffer{}, fmt.Errorf("Error while processing thumbnail: %s -> %v\n", file.FileName, err)
 	}
 
-	size := int64(buf.Len())
+	return buf, nil
+
+	
+}
+
+func processVideo(minioClient *minio.Client, db *gorm.DB, file models.File, assetFile *minio.Object, userBucket string) {
+	
+}
+
+func GenerateThumbnail(ctx context.Context, minioClient *minio.Client, db *gorm.DB, file models.File, userBucket string) {
+	assetFile, err := minioClient.GetObject(ctx, userBucket, file.FileCode, minio.GetObjectOptions{})
+
+	if err != nil {
+		log.Printf("Error while fetching file: %s -> %s\n", file.FileName, err.Error())
+		return
+	}
+	defer assetFile.Close()
+
+	var thumbnailBuf bytes.Buffer
+
+	if strings.HasPrefix(file.FileType, "image/") {
+		thumbnailBuf, err = processImage(file, assetFile, userBucket)
+		if err != nil {
+			log.Printf("Error while generating image thumbnail: %s -> %v\n", file.FileName, err)
+			return
+		}
+	} else if strings.HasPrefix(file.FileType, "video/") {
+		processVideo(minioClient, db, file, assetFile, userBucket)
+	}
+
+	size := int64(thumbnailBuf.Len())
 	thumbPath := fmt.Sprintf("/thumb/%s.jpg", file.FileCode)
 
-	_, err = minioClient.PutObject(ctx, userBucket, thumbPath, &buf, size, minio.PutObjectOptions{ContentType: "image/jpeg"})
+	_, err = minioClient.PutObject(ctx, userBucket, thumbPath, &thumbnailBuf, size, minio.PutObjectOptions{ContentType: "image/jpeg"})
 	if err != nil {
 		log.Printf("Error while uploading thumbnail: %s (%s) -> %s\n", thumbPath, file.FileName, err.Error())
 		return
 	}
 
 	thumbnail := models.Thumbnail{
-		FileID: file.ID,
+		FileID:   file.ID,
 		FilePath: thumbPath,
 	}
 
@@ -62,6 +84,6 @@ func GenerateThumbnail(ctx context.Context, minioClient *minio.Client, db *gorm.
 		log.Printf("Error while saving thumbnail: %s (%s) -> %s\n", thumbPath, file.FileName, err.Error())
 		return
 	}
-	
+
 	log.Printf("Thumbnail created: %s (%s)\n", thumbPath, file.FileName)
 }
