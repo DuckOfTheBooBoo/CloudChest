2fd20cda1131c98fe15ebe4e30842d1d74e71df0
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-08-13 12:17:55 +0000

    feat(backend): Use cookies for request that doesn't use axios

--- backend/middlewares/jwtMiddleware.go
+++ backend/middlewares/jwtMiddleware.go
@@ -4,25 +4,37 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/DuckOfTheBooBoo/web-gallery-app/backend/models"
+	_ "github.com/DuckOfTheBooBoo/web-gallery-app/backend/models"
 	"github.com/DuckOfTheBooBoo/web-gallery-app/backend/utils"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	_ "gorm.io/gorm"
 )
 
 func JWTMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		db := c.MustGet("db").(*gorm.DB)
+		// db := c.MustGet("db").(*gorm.DB)
 		tokenString := strings.ReplaceAll(c.GetHeader("Authorization"), "Bearer ", "")
+		tokenCookie, err := c.Cookie("token")
+
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		if tokenCookie != "" {
+			tokenString = tokenCookie
+		}
 
 		userClaims, err := utils.ParseToken(tokenString)
 		// Check if token is revoked (user has logged out, marking the token as invalid)
-		var token models.Token
-		if err := db.First(&token, "token = ?", tokenString); err != nil {
-			token = models.Token{}
-		}
+		// var token models.Token
+		// if err := db.First(&token, "token = ?", tokenString); err != nil {
+		// 	token = models.Token{}
+		// }
 
-		if err != nil || (models.Token{}) != token {
+		// if err != nil || (models.Token{}) != token {
+		if err != nil {
 			c.Status(http.StatusUnauthorized)
 			c.Abort()
 			return
